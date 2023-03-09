7dc53ddee4a1c2826d5b7c2c9be913bb59dadf99
Daniel <daniel.marques@edu.unifil.br>
2023-03-09 15:19:03 -0300

    organizing modules and pom

--- teste-gubee/teste-gubee-resource/src/main/java/br/com/gubee/resource/controllers/ProductResource.java
+++ teste-gubee/teste-gubee-resource/src/main/java/br/com/gubee/resource/controllers/ProductResource.java
@@ -1,4 +1,11 @@
 package br.com.gubee.resource.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gubee/product")
 public class ProductResource {
+
+
 }
